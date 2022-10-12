39c579b14726971ef56a7028ef13a7e714f91933
Rajendran Ramasamy <112932455+Rajendrankr25@users.noreply.github.com>
2022-10-12 12:19:36 +0530

    Add files via upload

---
+++ t2.js
@@ -0,0 +1,8 @@
+function disval(){
+    var fn=document.getElementById("fname").value;
+    var mn=document.getElementById("mname").value;
+    var ln=document.getElementById("lname").value;
+    var el=document.getElementById("email").value;
+    var ps=document.getElementById("pass").value;
+    console.log(fn, mn, ln, el, ps);
+}
